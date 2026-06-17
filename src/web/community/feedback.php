5b777a9788a272d825412c955b992d322935cab7
Hanad <hanadmse@gmail.com>
2026-06-17 12:30:21 -0500

    Add Chatbox CSS link to feedback page

--- src/web/community/feedback.php
+++ src/web/community/feedback.php
@@ -14,6 +14,7 @@ $isLoggedIn = isset($_SESSION['user_name']) || isset($_SESSION['admin_name']);
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Playfair+Display:wght@700;800;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/css/Chatbox.css">
   <link rel="stylesheet" href="../assets/css/Feedback.css">
   <link rel="stylesheet" href="../assets/css/Breadcrumb.css">
   <link rel="stylesheet" href="../assets/css/toolbar.css">
